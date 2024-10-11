452dd23755d94bc78623718ed57ad35f7ea6da52
tirth1207 <104121248+tirth1207@users.noreply.github.com>
2024-10-11 14:28:01 +0530

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -75,18 +75,18 @@ export const DATA = {
   },
 
   work: [
-    // {
-    //   company: "Atomic Finance",
-    //   href: "https://atomic.finance",
-    //   badges: [],
-    //   location: "Remote",
-    //   title: "Bitcoin Protocol Engineer",
-    //   logoUrl: "/atomic.png",
-    //   start: "May 2021",
-    //   end: "Oct 2022",
-    //   description:
-    //     "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
-    // },
+    {
+      company: "None",
+      href: "",
+      badges: [],
+      location: "",
+      title: "",
+      logoUrl: "",
+      start: "",
+      end: "",
+      description:
+        "",
+    },
     // {
     //   company: "Shopify",
     //   badges: [],
